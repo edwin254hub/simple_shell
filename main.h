0f38f3a3d1b0f340229237ba83a11f8e261f31af
edwin254hub <edwinmutunga42@gmail.com>
2023-04-27 11:14:46 -0700

    shell

--- main.h
+++ main.h
@@ -3,26 +3,73 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_CMD_LEN 120
-
+#define BUFFER 1024
+#define TRUE 1
+#define PROMPT "$ "
+/* error strings */
+#define ERR_MALLOC "Unable to malloc space\n"
+#define ERR_FORK "Unable to fork and create child process\n"
+#define ERR_PATH "No such file or directory\n"
 extern char **environ;
 
-char *tokenize_string(char *input_string, const char *separator);
-char **separate_args(char *cmd);
-char **get_cmd(void);
-void run_exec(char **args);
-char *locate_exec(char *cmd);
-int find_exec_path(char *directory_list, char *cmd, char *absolute_path);
-void terminate_shell(char **args);
-char *search_char(const char *target_str, int target_char);
-char *string_concat(char *output_str, const char *input_str);
-int string_length(const char *str);
-int string_compare(char *stringA, char *stringB);
-void string_copy(char *source, char *destination);
-char *duplicate_str(const char *str_source);
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-char *_strtok(char *str, const char *delim);
-#endif /*MAIN_H_*/
+/**
+ * struct list_s - linked list of variables
+ * @value: value
+ * @next: pointer to next node
+ *
+ * Description: generic linked list struct for variables.
+**/
+typedef struct list_s
+{
+	char *value;
+	struct list_s *next;
+} list_s;
+
+/**
+ * struct built_s - linked list of builtins
+ * @name: name of builtin
+ * @p: pointer to function
+ *
+ * Description: struct for builtin functions.
+**/
+typedef struct built_s
+{
+	char *name;
+	int (*p)(void);
+} built_s;
+
+void prompt(int fd, struct stat buf);
+char *_getline(FILE *fp);
+char **tokenizer(char *str);
+char *_which(char *command, char *fullpath, char *path);
+int child(char *fullpath, char **tokens);
+void errors(int error);
+
+/* utility functions */
+void _puts(char *str);
+int _strlen(char *s);
+int _strcmp(char *name, char *variable, unsigned int length);
+int _strncmp(char *name, char *variable, unsigned int length);
+char *_strcpy(char *dest, char *src);
+
+/* prototypes for builtins */
+int shell_env(void);
+int shell_exit(void);
+int builtin_execute(char **tokens);
+int shell_num_builtins(built_s builtin[]);
+
+/* prototypes for the helper functions for path linked list */
+char *_getenv(const char *name);
+char **copy_env(char **environ_copy, unsigned int environ_length);
+list_s *pathlist(char *variable, list_s *head);
+
+/* prototypes for free functions */
+void free_all(char **tokens, char *path, char *line, char *fullpath, int flag);
+void free_dp(char **array, unsigned int length);
+#endif /* MAIN_H_*/
